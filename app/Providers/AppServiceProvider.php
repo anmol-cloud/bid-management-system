315eedc98d6a091506f84a26ad6010d1513b156c
anmol-cloud <144055727+anmol-cloud@users.noreply.github.com>
2026-09-01 09:57:37 +0530

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,26 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
 
-public function boot(): void
+class AppServiceProvider extends ServiceProvider
 {
-    if (app()->environment('production')) {
-        URL::forceScheme('https');
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
